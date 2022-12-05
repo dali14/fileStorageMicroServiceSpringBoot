ff2f923bebebabc645e33211a16811cb8c52cf05
dali14 <dalijlasii@gmail.com>
2022-12-05 02:23:36 +0100

    image storage micro service

---
+++ src/test/java/org/sid/storageservice/StorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.storageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
